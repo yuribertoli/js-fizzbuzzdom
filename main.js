f46a6e0616670286c730938cbb8cdb620a1ba420
yuribertoli <yuribertoli@yahoo.it>
2022-01-23 20:01:00 +0100

    fizzbuzz

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ let containerDom = document.getElementById("container");
 let number; 
 
 for (let number=1; number<=100; number++) {
-    console.log(number);
-    containerDom.innerHTML = containerDom.innerHTML + "<div class='box'>" + number + "</div>"; 
+    if ((number % 5 == 0) && (number % 3 == 0)) {
+        containerDom.innerHTML = containerDom.innerHTML + "<div class='box box35'>" + "fizzbuzz" + "</div>";
+    }
+    else if (number % 3 == 0) {
+        containerDom.innerHTML = containerDom.innerHTML + "<div class='box box3'>" + "fizz" + "</div>";
+    }
+    else if (number % 5 == 0) {
+        containerDom.innerHTML = containerDom.innerHTML + "<div class='box box5'>" + "buzz" + "</div>"; 
+    }
+    else {
+        containerDom.innerHTML = containerDom.innerHTML + "<div class='box'>" + number + "</div>";
+    }
 }
+
+
+
