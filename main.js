7794ae176b10a0a69f08c665961aeab7a9dd322d
yuribertoli <yuribertoli@yahoo.it>
2022-01-23 18:58:04 +0100

    css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let containerDom = document.getElementById("container");
 let number; 
 
-for (let number=0; number<=100; number++) {
+for (let number=1; number<=100; number++) {
     console.log(number);
     containerDom.innerHTML = containerDom.innerHTML + "<div class='box'>" + number + "</div>"; 
 }
